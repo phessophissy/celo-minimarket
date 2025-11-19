7374245f61ed4b2a9356e93e702f92b257e10376
phessophissy <adefesobi@yahoo.com>
2025-11-19 21:48:51 +0100

    Deploy contract to mainnet and update frontend with contract address

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useContractKit } from '@celo-tools/use-contractkit'
 import { ethers } from 'ethers'
 import marketAbi from './abi/CeloMiniMarket.json'
 
-const MARKET_ADDRESS = 'PASTE_DEPLOYED_MAINNET_ADDRESS'
+const MARKET_ADDRESS = '0x1C824627899cFaeB4bb68101efa022917c93b923'
 const CUSD_ADDRESS   = '0x765DE816845861e75A25fCA122bb6898B8B1282a'
 
 const erc20Abi = [
