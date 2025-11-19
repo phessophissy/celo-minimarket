842a9a8b9ef703634529e22f4d864755860faa1a
phessophissy <adefesobi@yahoo.com>
2025-11-19 22:31:13 +0100

    Add error boundary component

---
+++ frontend/src/ErrorBoundary.jsx
@@ -0,0 +1,14 @@
+import React from react
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false, error: null }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(ErrorBoundary
