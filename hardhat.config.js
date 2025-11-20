20ae4ac76b140ded092fd9fb1154c7b84dffb2e4
phessophissy <adefesobi@yahoo.com>
2025-11-20 11:12:32 +0100

    Add fallback RPC and make Add button smaller/sleeker

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
-import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-verify';
 import 'dotenv/config';
 
 export default {
-  solidity: '0.8.20',
+  solidity: {
+    version: '0.8.20',
+    settings: {
+      evmVersion: 'paris',
+      optimizer: {
+        enabled: false,
+        runs: 200
+      }
+    }
+  },
   networks: {
     celo: {
       url: process.env.CELO_MAINNET_RPC,
@@ -10,7 +19,24 @@ export default {
       chainId: 42220,
     },
   },
+  sourcify: {
+    enabled: false
+  },
   etherscan: {
-    apiKey: 'CELOSCAN_API_KEY',
+    apiKey: {
+      celo: 'CELOSCAN_API_KEY'
+    },
+    customChains: [
+      {
+        network: 'celo',
+        chainId: 42220,
+        urls: {
+          apiURL: 'https://api.celoscan.io/api',
+          browserURL: 'https://celoscan.io'
+        }
+      }
+    ]
   }
 };
+
+
