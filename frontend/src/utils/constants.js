0a73c484856623abdb6112ab2265eb305c83d2d4
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-17 00:48:46 +0100

    feat: add centralized Celo constants and addresses

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,14 @@
+export const MARKET_ADDRESS = '0x7a280e8b5995F72F20a1e90177C20D002aC1C3a4';
+export const CUSD_ADDRESS = '0x765DE816845861e75A25fCA122bb6898B8B1282a';
+export const CELO_RPC_URL = 'https://rpc.ankr.com/celo';
+export const CELO_EXPLORER = 'https://explorer.celo.org/mainnet';
+export const CELO_CHAIN_ID = 42220;
+
+export const CELO_COLORS = {
+  green: '#35D07F',
+  gold: '#FBCC5C',
+  dark: '#2E3338',
+  violet: '#BF97FF',
+  red: '#FB7C6D',
+  blue: '#3488EC',
+};
