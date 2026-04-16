eed2efacf287ddfb3e4f933a54b7a3786e67425b
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-17 00:48:46 +0100

    feat: add CeloBadge SVG component with Celo logo

---
+++ frontend/src/components/CeloBadge.jsx
@@ -0,0 +1,11 @@
+export default function CeloBadge() {
+  return (
+    <div className="celo-badge">
+      <svg width="16" height="16" viewBox="0 0 950 950" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="475" cy="475" r="475" fill="#35D07F"/>
+        <path d="M475 150C295.5 150 150 295.5 150 475C150 654.5 295.5 800 475 800C573 800 660.5 756 720 687.5C680.5 738 616.5 770 545 770C403 770 288 655 288 513C288 371 403 256 545 256C616.5 256 680.5 288 720 338.5C660.5 270 573 150 475 150Z" fill="white"/>
+      </svg>
+      <span>Powered by Celo</span>
+    </div>
+  );
+}
