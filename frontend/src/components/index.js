6e7216e285e7990819128cad9dd72d5557afdf33
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:02:25 +0100

    refactor: export views components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as ViewToggle } from './ViewToggle.jsx';
+export { default as ListView } from './ListView.jsx';
+export { default as GridView } from './GridView.jsx';
