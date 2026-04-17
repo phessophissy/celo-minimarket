cad1fd460fa433d7eeabc8b6e5044df1854e0bc0
phessophissy <adefesobi@yahoo.com>
2026-04-17 01:58:48 +0100

    refactor: export images components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as OptimizedImage } from './OptimizedImage.jsx';
+export { default as ImagePreview } from './ImagePreview.jsx';
+export { default as ImagePlaceholder } from './ImagePlaceholder.jsx';
