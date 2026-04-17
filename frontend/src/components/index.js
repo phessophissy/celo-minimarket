24800fc0ca1a048af9669689d9fafe3f8b0f7a95
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:06:19 +0100

    refactor: export ratelimit components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as RateLimitGuard } from './RateLimitGuard.jsx';
+export { default as ThrottledButton } from './ThrottledButton.jsx';
+export { default as CooldownIndicator } from './CooldownIndicator.jsx';
