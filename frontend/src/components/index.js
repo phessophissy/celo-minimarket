e199ac3f37d89062b6bdc7f187c86e3978d9e3c2
phessophissy <adefesobi@yahoo.com>
2026-04-17 01:54:04 +0100

    refactor: export responsive components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as MobileMenu } from './MobileMenu.jsx';
+export { default as TouchHandler } from './TouchHandler.jsx';
+export { default as SwipeDetector } from './SwipeDetector.jsx';
