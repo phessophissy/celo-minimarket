b75713f01aa3459dc532c911535ac4bde0cb8ce5
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-17 07:43:16 +0100

    feat: add batch product operations (#39)
    * feat: add batchOps for batch feature module
    
    * feat: add BatchSelector for batch feature module
    
    * feat: add BatchActions for batch feature module
    
    * feat: add batchUtils for batch feature module
    
    * feat: add BatchProgress for batch feature module
    
    * refactor: export batch utilities from utils index
    
    * refactor: export batch components from barrel file
    
    * feat: add useBatch hook for batch state management
    
    * refactor: export useBatch from hooks barrel file
    
    * docs: add batch feature documentation

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -14,6 +14,9 @@ export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
 
+export { default as BatchSelector } from './BatchSelector.jsx';
+export { default as BatchActions } from './BatchActions.jsx';
+export { default as BatchProgress } from './BatchProgress.jsx';
 export { default as LanguagePicker } from './LanguagePicker.jsx';
 export { default as TranslatedText } from './TranslatedText.jsx';
 export { default as LocaleProvider } from './LocaleProvider.jsx';
