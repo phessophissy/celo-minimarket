6d711d50e9374d5ddac59b5e2a0cf3cc646aab3e
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:05:58 +0100

    refactor: export address components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as AddressCopyButton } from './AddressCopyButton.jsx';
+export { default as TruncatedAddress } from './TruncatedAddress.jsx';
+export { default as AddressDisplay } from './AddressDisplay.jsx';
