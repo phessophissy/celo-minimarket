3d63eba78f2431f4380d20eb05d60e209fe4a98c
phessophissy <adefesobi@yahoo.com>
2026-04-17 01:59:12 +0100

    refactor: export events components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as EventFeed } from './EventFeed.jsx';
+export { default as EventListener } from './EventListener.jsx';
+export { default as LiveIndicator } from './LiveIndicator.jsx';
