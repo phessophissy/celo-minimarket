65ae25842dfd2e7229d52d4f41e8544607444800
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:02:05 +0100

    refactor: export network components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as NetworkIndicator } from './NetworkIndicator.jsx';
+export { default as OfflineBanner } from './OfflineBanner.jsx';
+export { default as ConnectionQuality } from './ConnectionQuality.jsx';
