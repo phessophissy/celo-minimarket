fccf0ffcbdfa4ca1548f1320534e0f823881ac8e
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:02:43 +0100

    refactor: export logging components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as LogViewer } from './LogViewer.jsx';
+export { default as LogEntry } from './LogEntry.jsx';
+export { default as LogExport } from './LogExport.jsx';
