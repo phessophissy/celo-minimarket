4e7335c8601242efef16c47f36a7784b893f2d0e
phessophissy <adefesobi@yahoo.com>
2026-04-17 01:58:25 +0100

    refactor: export sharing components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as ShareMenu } from './ShareMenu.jsx';
+export { default as ProductShareButton } from './ProductShareButton.jsx';
+export { default as SocialLinks } from './SocialLinks.jsx';
