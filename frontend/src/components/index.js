6d9edbb4ea4e06632f36f7f30781daf4dd1ad86d
phessophissy <adefesobi@yahoo.com>
2026-04-17 01:59:44 +0100

    refactor: export detail components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as ProductDetailModal } from './ProductDetailModal.jsx';
+export { default as ProductMeta } from './ProductMeta.jsx';
+export { default as FullScreenImage } from './FullScreenImage.jsx';
