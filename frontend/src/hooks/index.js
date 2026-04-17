9340cef030cce0acd96941e991fdee01cfb19fca
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:06:00 +0100

    refactor: export useAddress from hooks barrel file

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,5 @@
 export { default as useCopyToClipboard } from './useCopyToClipboard.js';
 export { default as useLocalStorage } from './useLocalStorage.js';
 export { default as useMediaQuery } from './useMediaQuery.js';
+
+export { default as useAddress } from './useAddress.js';
