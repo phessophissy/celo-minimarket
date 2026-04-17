fd61d2c356a4034260509a2e68297912115b76ca
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:00:16 +0100

    refactor: export pagination components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as Pagination } from './Pagination.jsx';
+export { default as InfiniteScroll } from './InfiniteScroll.jsx';
+export { default as PageSizeSelector } from './PageSizeSelector.jsx';
