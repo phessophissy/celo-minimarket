12dff53f3a017866e2decefdfc4db2d7fbaf8326
phessophissy <adefesobi@yahoo.com>
2026-04-17 02:09:59 +0100

    refactor: export receipt components from barrel file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -13,3 +13,7 @@ export { default as ConfirmModal } from './ConfirmModal.jsx';
 export { default as ScrollTop } from './ScrollTop.jsx';
 export { default as CeloBadge } from './CeloBadge.jsx';
 export { default as Skeleton } from './Skeleton.jsx';
+
+export { default as ReceiptModal } from './ReceiptModal.jsx';
+export { default as ReceiptPrint } from './ReceiptPrint.jsx';
+export { default as ReceiptDownload } from './ReceiptDownload.jsx';
