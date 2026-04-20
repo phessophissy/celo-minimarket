f869b73796cad06c2a7c4a419d8b9ddd2275c58f
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:47:47 +0100

    feat: add barrel export for notification system modules

---
+++ frontend/src/utils/notify/index.js
@@ -0,0 +1,10 @@
+// Notification system - barrel export
+export { createNotifyHandler1, transformNotifyData1 } from './module1.js';
+export { createNotifyHandler2, transformNotifyData2 } from './module2.js';
+export { createNotifyHandler3, transformNotifyData3 } from './module3.js';
+export { createNotifyHandler4, transformNotifyData4 } from './module4.js';
+export { createNotifyHandler5, transformNotifyData5 } from './module5.js';
+export { createNotifyHandler6, transformNotifyData6 } from './module6.js';
+export { createNotifyHandler7, transformNotifyData7 } from './module7.js';
+export { createNotifyHandler8, transformNotifyData8 } from './module8.js';
+export { createNotifyHandler9, transformNotifyData9 } from './module9.js';
