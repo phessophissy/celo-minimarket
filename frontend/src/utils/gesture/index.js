e04776f670c22929f20376f1e3b4ae7234b88248
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:49:55 +0100

    feat: add barrel export for gesture handling modules

---
+++ frontend/src/utils/gesture/index.js
@@ -0,0 +1,10 @@
+// Gesture handling - barrel export
+export { createGestureHandler1, transformGestureData1 } from './module1.js';
+export { createGestureHandler2, transformGestureData2 } from './module2.js';
+export { createGestureHandler3, transformGestureData3 } from './module3.js';
+export { createGestureHandler4, transformGestureData4 } from './module4.js';
+export { createGestureHandler5, transformGestureData5 } from './module5.js';
+export { createGestureHandler6, transformGestureData6 } from './module6.js';
+export { createGestureHandler7, transformGestureData7 } from './module7.js';
+export { createGestureHandler8, transformGestureData8 } from './module8.js';
+export { createGestureHandler9, transformGestureData9 } from './module9.js';
