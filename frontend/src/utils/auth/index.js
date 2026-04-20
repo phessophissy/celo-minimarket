2425dbc41addf91a93e534ce4c0cd785e3fe3a07
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:48:00 +0100

    feat: add barrel export for authentication flow modules

---
+++ frontend/src/utils/auth/index.js
@@ -0,0 +1,10 @@
+// Authentication flow - barrel export
+export { createAuthHandler1, transformAuthData1 } from './module1.js';
+export { createAuthHandler2, transformAuthData2 } from './module2.js';
+export { createAuthHandler3, transformAuthData3 } from './module3.js';
+export { createAuthHandler4, transformAuthData4 } from './module4.js';
+export { createAuthHandler5, transformAuthData5 } from './module5.js';
+export { createAuthHandler6, transformAuthData6 } from './module6.js';
+export { createAuthHandler7, transformAuthData7 } from './module7.js';
+export { createAuthHandler8, transformAuthData8 } from './module8.js';
+export { createAuthHandler9, transformAuthData9 } from './module9.js';
