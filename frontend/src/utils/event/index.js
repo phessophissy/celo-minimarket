0c2195e3a1ea946def1b23e29e90a117d3daacc7
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:49:06 +0100

    feat: add barrel export for event handling modules

---
+++ frontend/src/utils/event/index.js
@@ -0,0 +1,10 @@
+// Event handling - barrel export
+export { createEventHandler1, transformEventData1 } from './module1.js';
+export { createEventHandler2, transformEventData2 } from './module2.js';
+export { createEventHandler3, transformEventData3 } from './module3.js';
+export { createEventHandler4, transformEventData4 } from './module4.js';
+export { createEventHandler5, transformEventData5 } from './module5.js';
+export { createEventHandler6, transformEventData6 } from './module6.js';
+export { createEventHandler7, transformEventData7 } from './module7.js';
+export { createEventHandler8, transformEventData8 } from './module8.js';
+export { createEventHandler9, transformEventData9 } from './module9.js';
