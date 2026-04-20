aa1891a8153f56152bb8457bcbb192c7ab7a65e5
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:49:00 +0100

    feat: add barrel export for state management modules

---
+++ frontend/src/utils/state/index.js
@@ -0,0 +1,10 @@
+// State management - barrel export
+export { createStateHandler1, transformStateData1 } from './module1.js';
+export { createStateHandler2, transformStateData2 } from './module2.js';
+export { createStateHandler3, transformStateData3 } from './module3.js';
+export { createStateHandler4, transformStateData4 } from './module4.js';
+export { createStateHandler5, transformStateData5 } from './module5.js';
+export { createStateHandler6, transformStateData6 } from './module6.js';
+export { createStateHandler7, transformStateData7 } from './module7.js';
+export { createStateHandler8, transformStateData8 } from './module8.js';
+export { createStateHandler9, transformStateData9 } from './module9.js';
