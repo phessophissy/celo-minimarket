8f4a92fb4a696c7f0dd820fd346940ac7b19e527
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:43:18 +0100

    feat: add barrel export for market analytics modules

---
+++ frontend/src/utils/market/index.js
@@ -0,0 +1,10 @@
+// Market analytics - barrel export
+export { createMarketHandler1, transformMarketData1 } from './module1.js';
+export { createMarketHandler2, transformMarketData2 } from './module2.js';
+export { createMarketHandler3, transformMarketData3 } from './module3.js';
+export { createMarketHandler4, transformMarketData4 } from './module4.js';
+export { createMarketHandler5, transformMarketData5 } from './module5.js';
+export { createMarketHandler6, transformMarketData6 } from './module6.js';
+export { createMarketHandler7, transformMarketData7 } from './module7.js';
+export { createMarketHandler8, transformMarketData8 } from './module8.js';
+export { createMarketHandler9, transformMarketData9 } from './module9.js';
