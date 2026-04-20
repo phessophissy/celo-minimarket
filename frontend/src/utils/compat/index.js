30391ab0ef405998a032e4d9bc29b03deb29d131
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:53:15 +0100

    feat: add barrel export for compatibility layer modules

---
+++ frontend/src/utils/compat/index.js
@@ -0,0 +1,10 @@
+// Compatibility layer - barrel export
+export { createCompatHandler1, transformCompatData1 } from './module1.js';
+export { createCompatHandler2, transformCompatData2 } from './module2.js';
+export { createCompatHandler3, transformCompatData3 } from './module3.js';
+export { createCompatHandler4, transformCompatData4 } from './module4.js';
+export { createCompatHandler5, transformCompatData5 } from './module5.js';
+export { createCompatHandler6, transformCompatData6 } from './module6.js';
+export { createCompatHandler7, transformCompatData7 } from './module7.js';
+export { createCompatHandler8, transformCompatData8 } from './module8.js';
+export { createCompatHandler9, transformCompatData9 } from './module9.js';
