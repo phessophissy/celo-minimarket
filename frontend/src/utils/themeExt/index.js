e0e236bf8f3bc3472d94266e8c032f0493eb6d83
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:49:29 +0100

    feat: add barrel export for theme extension modules

---
+++ frontend/src/utils/themeExt/index.js
@@ -0,0 +1,10 @@
+// Theme extension - barrel export
+export { createThemeExtHandler1, transformThemeExtData1 } from './module1.js';
+export { createThemeExtHandler2, transformThemeExtData2 } from './module2.js';
+export { createThemeExtHandler3, transformThemeExtData3 } from './module3.js';
+export { createThemeExtHandler4, transformThemeExtData4 } from './module4.js';
+export { createThemeExtHandler5, transformThemeExtData5 } from './module5.js';
+export { createThemeExtHandler6, transformThemeExtData6 } from './module6.js';
+export { createThemeExtHandler7, transformThemeExtData7 } from './module7.js';
+export { createThemeExtHandler8, transformThemeExtData8 } from './module8.js';
+export { createThemeExtHandler9, transformThemeExtData9 } from './module9.js';
