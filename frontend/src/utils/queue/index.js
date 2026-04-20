788085a4d51322e3ea64b154d7539e6ff91f1404
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:47:24 +0100

    feat: add barrel export for queue processing modules

---
+++ frontend/src/utils/queue/index.js
@@ -0,0 +1,10 @@
+// Queue processing - barrel export
+export { createQueueHandler1, transformQueueData1 } from './module1.js';
+export { createQueueHandler2, transformQueueData2 } from './module2.js';
+export { createQueueHandler3, transformQueueData3 } from './module3.js';
+export { createQueueHandler4, transformQueueData4 } from './module4.js';
+export { createQueueHandler5, transformQueueData5 } from './module5.js';
+export { createQueueHandler6, transformQueueData6 } from './module6.js';
+export { createQueueHandler7, transformQueueData7 } from './module7.js';
+export { createQueueHandler8, transformQueueData8 } from './module8.js';
+export { createQueueHandler9, transformQueueData9 } from './module9.js';
