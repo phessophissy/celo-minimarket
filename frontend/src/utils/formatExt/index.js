b4c97c5691e2ce2a5938381ab8584668dcf4bf1f
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:50:57 +0100

    feat: add barrel export for format extensions modules

---
+++ frontend/src/utils/formatExt/index.js
@@ -0,0 +1,10 @@
+// Format extensions - barrel export
+export { createFormatExtHandler1, transformFormatExtData1 } from './module1.js';
+export { createFormatExtHandler2, transformFormatExtData2 } from './module2.js';
+export { createFormatExtHandler3, transformFormatExtData3 } from './module3.js';
+export { createFormatExtHandler4, transformFormatExtData4 } from './module4.js';
+export { createFormatExtHandler5, transformFormatExtData5 } from './module5.js';
+export { createFormatExtHandler6, transformFormatExtData6 } from './module6.js';
+export { createFormatExtHandler7, transformFormatExtData7 } from './module7.js';
+export { createFormatExtHandler8, transformFormatExtData8 } from './module8.js';
+export { createFormatExtHandler9, transformFormatExtData9 } from './module9.js';
