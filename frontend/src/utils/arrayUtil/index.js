6b80df522ca8c584dbd1f944412ac7fd4befdb0b
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:51:24 +0100

    feat: add barrel export for array operations modules

---
+++ frontend/src/utils/arrayUtil/index.js
@@ -0,0 +1,10 @@
+// Array operations - barrel export
+export { createArrayUtilHandler1, transformArrayUtilData1 } from './module1.js';
+export { createArrayUtilHandler2, transformArrayUtilData2 } from './module2.js';
+export { createArrayUtilHandler3, transformArrayUtilData3 } from './module3.js';
+export { createArrayUtilHandler4, transformArrayUtilData4 } from './module4.js';
+export { createArrayUtilHandler5, transformArrayUtilData5 } from './module5.js';
+export { createArrayUtilHandler6, transformArrayUtilData6 } from './module6.js';
+export { createArrayUtilHandler7, transformArrayUtilData7 } from './module7.js';
+export { createArrayUtilHandler8, transformArrayUtilData8 } from './module8.js';
+export { createArrayUtilHandler9, transformArrayUtilData9 } from './module9.js';
