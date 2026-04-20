bb1f8cc2a87c940dab51470b8c5b9e2cc6a5937b
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:49:37 +0100

    feat: add barrel export for animation utilities modules

---
+++ frontend/src/utils/animUtil/index.js
@@ -0,0 +1,10 @@
+// Animation utilities - barrel export
+export { createAnimUtilHandler1, transformAnimUtilData1 } from './module1.js';
+export { createAnimUtilHandler2, transformAnimUtilData2 } from './module2.js';
+export { createAnimUtilHandler3, transformAnimUtilData3 } from './module3.js';
+export { createAnimUtilHandler4, transformAnimUtilData4 } from './module4.js';
+export { createAnimUtilHandler5, transformAnimUtilData5 } from './module5.js';
+export { createAnimUtilHandler6, transformAnimUtilData6 } from './module6.js';
+export { createAnimUtilHandler7, transformAnimUtilData7 } from './module7.js';
+export { createAnimUtilHandler8, transformAnimUtilData8 } from './module8.js';
+export { createAnimUtilHandler9, transformAnimUtilData9 } from './module9.js';
