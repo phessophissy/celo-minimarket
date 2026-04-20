ff4fbb28133f6588075161fd7d7708d4ec81fb48
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:52:12 +0100

    feat: add barrel export for debug utilities modules

---
+++ frontend/src/utils/debugUtil/index.js
@@ -0,0 +1,10 @@
+// Debug utilities - barrel export
+export { createDebugUtilHandler1, transformDebugUtilData1 } from './module1.js';
+export { createDebugUtilHandler2, transformDebugUtilData2 } from './module2.js';
+export { createDebugUtilHandler3, transformDebugUtilData3 } from './module3.js';
+export { createDebugUtilHandler4, transformDebugUtilData4 } from './module4.js';
+export { createDebugUtilHandler5, transformDebugUtilData5 } from './module5.js';
+export { createDebugUtilHandler6, transformDebugUtilData6 } from './module6.js';
+export { createDebugUtilHandler7, transformDebugUtilData7 } from './module7.js';
+export { createDebugUtilHandler8, transformDebugUtilData8 } from './module8.js';
+export { createDebugUtilHandler9, transformDebugUtilData9 } from './module9.js';
