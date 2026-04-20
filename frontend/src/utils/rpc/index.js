2cd0d405881fdd7e5d88e76ad0ee6d162475ce9a
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 07:48:53 +0100

    feat: add barrel export for rpc communication modules

---
+++ frontend/src/utils/rpc/index.js
@@ -0,0 +1,10 @@
+// RPC communication - barrel export
+export { createRpcHandler1, transformRpcData1 } from './module1.js';
+export { createRpcHandler2, transformRpcData2 } from './module2.js';
+export { createRpcHandler3, transformRpcData3 } from './module3.js';
+export { createRpcHandler4, transformRpcData4 } from './module4.js';
+export { createRpcHandler5, transformRpcData5 } from './module5.js';
+export { createRpcHandler6, transformRpcData6 } from './module6.js';
+export { createRpcHandler7, transformRpcData7 } from './module7.js';
+export { createRpcHandler8, transformRpcData8 } from './module8.js';
+export { createRpcHandler9, transformRpcData9 } from './module9.js';
